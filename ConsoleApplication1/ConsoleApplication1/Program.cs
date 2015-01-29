587d9e7fd89116afa0ccecc7fb70fc0cb153f106
davidss20 <davidss20@gmail.com>
2015-01-29 19:07:26 +0200

    add my func

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Mordechai was here!!!");
+            Console.WriteLine("David was here!!!");
         }
     }
 }
